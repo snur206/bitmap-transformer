8bb4d3698f51e2324dd7a2ba7eae55fe407c83ee
SHARMARKE NUR <Snur206@yahoo.com>
2023-01-12 19:48:11 -0800

    lab 4

--- app/src/main/java/bitmap/transformer/App.java
+++ app/src/main/java/bitmap/transformer/App.java
@@ -3,12 +3,43 @@
  */
 package bitmap.transformer;
 
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException {
+        File file = new File("app//src//main//resources//baldy-8bit.bmp");
+        BufferedImage inputFileImg = ImageIO.read(file);
         System.out.println(new App().getGreeting());
+        int height = inputFileImg.getHeight();
+        int width = inputFileImg.getWidth();
+        File outFileImage = new File(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        public void write(String[] file) {
+            ImageIO.write(inputFileImg, "bmp", outFileImage);
+        }
+
+        System.out.println(height + "  " + width + " " + img.getRGB(30, 30));
+
+        for (int h = 1; h < height; h++) {
+            for (int w = 1; w < width; w++) {
+                int rgb = inputFileImg.getRGB(w, h);
+                int red = (rgb >> 16) & 0x000000FF;
+                int green = (rgb >> 8) & 0x000000FF;
+                int blue = (rgb) & 0x000000FF;
+
+                if (red == 0 && green == 0 && blue == 0) {
+                    System.out.println("Black");
+                }
+            }
+        }
+
     }
 }
+
